0f3507101328c7529a8006d94b7985437dc454f0
Parallels <aspain2394@gmailom>
2018-10-11 15:55:10 -0400

    setup catkin workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/parallels/Extended-Kalman-ROS/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/parallels/catkin_ws/devel;/home/parallels/turtlebot/devel;/home/parallels/kobuki/devel;/home/parallels/rocon/devel;/opt/ros/kinetic".split(';') if "/home/parallels/catkin_ws/devel;/home/parallels/turtlebot/devel;/home/parallels/kobuki/devel;/home/parallels/rocon/devel;/opt/ros/kinetic" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/parallels/Extended-Kalman-ROS/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/parallels/Extended-Kalman-ROS/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/parallels/Extended-Kalman-ROS/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/parallels/Extended-Kalman-ROS/src:$ROS_PACKAGE_PATH"
