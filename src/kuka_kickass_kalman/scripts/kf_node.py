c2723dc0c6f1fd3abb61418b57684d3cba262023
BenWare144 <37714013+BenWare144@users.noreply.github.com>
2018-10-15 15:43:13 -0400

    Update kf_node.py

--- src/kuka_kickass_kalman/scripts/kf_node.py
+++ src/kuka_kickass_kalman/scripts/kf_node.py
@@ -3,7 +3,7 @@ import rospy
 import time
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
-from kuka_kickass_kalman.scripts import ros_kalman_filter2
+import ros_kalman_filter2
 from kuka_kickass_kalman.msg import Obs
 
 def callback(vel_msg):
